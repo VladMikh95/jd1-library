a3f108f895456ea52c5f1871caeb5f33170447d6
VladMikh95 <51461063+VladMikh95@users.noreply.github.com>
2020-03-23 00:55:41 +0300

    Add files via upload

--- jd1-library/src/by/htp/library/dao/impl/FileUserDao.java
+++ jd1-library/src/by/htp/library/dao/impl/FileUserDao.java
@@ -1,9 +1,12 @@
 package by.htp.library.dao.impl;
 
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import by.htp.library.bean.User;
 import by.htp.library.dao.DaoException;
@@ -13,53 +16,72 @@ public class FileUserDao implements UserDao{
 	
 	@Override
 	public boolean authorization(String login, String password) throws DaoException {
-		if (login == null || login.isEmpty()) {
-			throw new RuntimeException("error");// stub
-		}
-		try {
-			FileReader fr = new FileReader("src\\resources\\users.txt");
-            BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            while (line != null) {
-            	if ((line.contains("login=" + login + ",")) 
-            			&& (line.contains("password=" + password + ","))) {
-            		return true;
-            	}
-                line = reader.readLine();
-            }
-			return false;
-			
-		} catch (IOException e) {
-			throw new DaoException(e);
-		}
+			for (User user : getUsersFromFile()) {
+				if ((login.equals(user.getLogin())) && (password.equals(user.getPassword()))) {
+					return true;
+				}
+			}
+			return false;	
+
 	}
 
 	@Override
 	public boolean registration(User newUser) throws DaoException{
-		if (newUser == null) {
-			throw new RuntimeException("error");// stub
-		}
+		FileWriter writer = null;
 		try {
-			int id = findLastLine(new FileReader("src\\resources\\users.txt")) + 1;
-			FileWriter writer = new FileWriter("src\\resources\\users.txt", true);
-			writer.write("id=" + id + " ");
+			int id = numberOfLastLine(new FileReader("resources\\users.txt")) + 1;
+			newUser.setId(id);
+			writer = new FileWriter("resources\\users.txt", true);
 			writer.write(newUser.toString());
 			writer.append('\n');
-			writer.close();
 			return true;
 			
 		} catch (IOException e) {
 			throw new DaoException(e);
+		} finally {
+			try {
+				if (writer != null) {
+					writer.close();
+				}
+			} catch(IOException e) {
+				throw new DaoException(e);
+			}
 		}
 	}
 
 	@Override
-	public boolean changeUserStatus(int idUser, String newStatus) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean changeUserStatus(int idUser, String newStatus) throws DaoException {
+		FileWriter writer = null;
+		
+		try {
+			boolean result = false;
+			List<User> users = getUsersFromFile();
+			writer = new FileWriter("resources\\users.txt");
+            for (User user : users) {
+            	if (idUser == user.getId()) {
+            		user.setStatus(newStatus);
+            		result = true;
+            	}
+            	writer.write(user.toString());
+            	writer.append('\n');
+            }
+			return result;
+			
+		} catch (IOException e) {
+			throw new DaoException(e);
+		} finally {
+			try {
+				if(writer != null) {
+					writer.close();
+				}
+			}  catch (IOException e) {
+				throw new DaoException(e);
+			}
+			
+		}
 	}
 	
-	private int findLastLine(FileReader fr) throws DaoException {
+	private int numberOfLastLine(FileReader fr) throws DaoException {
 		try {
 			int result = 0;
             BufferedReader reader = new BufferedReader(fr);
@@ -73,6 +95,41 @@ public class FileUserDao implements UserDao{
 		} catch (IOException e) {
 			throw new DaoException(e);
 		}
+	}
+	
+	private List<User> getUsersFromFile() throws DaoException {
+		List<User> users = new ArrayList<User>();
+		BufferedReader reader = null;
+		
+		try {
+            reader = new BufferedReader(new FileReader("resources\\users.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+            	String[] strings = line.split("' ");
+            	int id = Integer.parseInt(strings[0].substring(strings[0].indexOf("=") + 1));
+            	String login = strings[1].substring(strings[1].indexOf("=") + 1);
+            	String password = strings[2].substring(strings[2].indexOf("=") + 1);
+            	String name = strings[3].substring(strings[3].indexOf("=") + 1);
+            	String role = strings[4].substring(strings[4].indexOf("=") + 1);
+            	String status = strings[5].substring(strings[5].indexOf("=") + 1);
+            	users.add(new User(id, login, password, name, role, status));
+                line = reader.readLine();
+            }
+            return users;
+		} catch (IOException e) {
+			throw new DaoException(e);
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+				
+			} catch(IOException e){
+				throw new DaoException(e);
+			}
+		}
+
+		
 	}
 
 }
--- jd1-library/src/by/htp/library/start/Main.java
+++ jd1-library/src/by/htp/library/start/Main.java
@@ -1,16 +1,17 @@
 package by.htp.library.start;
 
-import by.htp.library.bean.User;
+import by.htp.library.bean.*;
 import by.htp.library.controller.Controller;
 import by.htp.library.dao.DaoException;
+import by.htp.library.dao.impl.FileAdminDao;
 import by.htp.library.dao.impl.FileUserDao;
 
 public class Main {
 
 	public static void main(String[] args) throws DaoException {
-		/*Controller controller = new Controller();
+		Controller controller = new Controller();
 		
-		String request, response;
+		/*String request, response;
 		
 		request = "login=aaa password=bbb name=Ivan";
 
@@ -18,13 +19,19 @@ public class Main {
 		
 		System.out.println(response);*/
 		
-		User user = new User("aaa", "bbb", "ccc", "ddd");
-		
 		FileUserDao fl = new FileUserDao();
+		
+		User user = new User("aab", "bbb", "ccc", "ddd");
+	
 		fl.registration(user);
 		System.out.println(fl.authorization("aaa", "bbb"));
+		System.out.println(fl.changeUserStatus(2, "blocked"));
 		
 		
+		FileAdminDao fa = new FileAdminDao();
+		Book book = new Book("Бейтс", "Изучаем Java", "paper", 50);
+		fa.add(book);
+		System.out.println(fa.edit(2, new Book("Эккель", "Философия Java", "paper, dig", 40)));
 	}
 
 }
